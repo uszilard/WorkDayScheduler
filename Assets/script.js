24d5c49ec813ee5a52f3ffbb080986e98640e169
Szilard Udroiu <uszilard@gmail.com>
2019-10-20 20:36:37 +0200

    initial commit

---
+++ Assets/script.js
@@ -0,0 +1,153 @@
+
+console.log("Script is here");
+
+$(document).ready(function () {
+
+    //current date and time
+    var m = moment();
+    var currentTime = m.format('MMMM Do YYYY, h:mm:ss a');
+    var currentDate = m.format("MMM Do YYYY");
+    var currentHour = moment().hours();
+
+    console.log(m);
+    console.log(currentTime);
+    console.log(currentHour);
+
+    //current date header
+    $("#currentDay").text("Today's Date: " + currentDate);
+
+    //timeblocks -----
+    //make 9am - 5pm timeslots
+    //make an array of timestamps
+    var timeStamp = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+    var hour = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
+
+    //loop and create divs
+    for (var i = 0; i < timeStamp.length; i++) {
+
+        var makeEventButton = $(`<button class='saveBtn' data-value = ' ${hour[i]}'>`)
+        var timeDiv = $("<div class='col-2'>" + timeStamp[i] + "</div>");
+        var eventInputField = $(`<input type='text' class='event-input' data-value = ' ${hour[i]} '>`);
+        var eventDiv = $(`<div class='col-8' data-value = ' ${hour[i]} '></div>`);
+        var savedEvents = $(`<div data value= ' ${hour[i]}'></div>`);
+
+        makeEventButton.text("Add Event");
+
+        //add styling to divs
+        $(timeDiv).addClass("time-block");
+        $(eventInputField).addClass("textarea");
+        $(eventDiv).addClass("event-div");
+        $(savedEvents).addClass("saved-events");
+
+        //add data-values for each div
+        var hourValue = hour[i];
+        $(".event-input").attr("data-value", timeStamp[i]);
+        $(savedEvents).data({ "hour": hourValue });
+        $(makeEventButton).data({ "hour": hourValue });
+        $(eventDiv).data({ "hour": hourValue });
+
+        console.log(timeStamp[i], eventDiv.attr("data-value"));
+
+        //append divs
+        $(".row").append(timeDiv);
+        $(".row").append(eventDiv);
+        $(".row").append(makeEventButton);
+        $(eventDiv).append(eventInputField);
+        $(eventDiv).append(savedEvents);
+
+    };
+
+    //save button listener for input to add to local storage
+    $(".saveBtn").on("click", function () {
+        // event.preventDefault();
+        // var eventTime = $(".event-button").attr();
+
+        //grab the text from the input box
+
+        var eventInput = $(".event-input").val();
+
+
+
+        //store text in local storage
+        var i = 0;
+        localStorage.setItem(i = timeStamp[i], eventInput);
+        console.log("~~~~adding event to local storage~~~~");
+        console.log($(".saveBtn").attr("data-value"));
+
+
+        //function to get event from local storage and render it on page
+        function renderEvent() {
+
+            $(".saved-events").append(eventInput);
+            console.log("render this event!!!");
+            console.log("event added: " + eventInput);
+        };
+
+        //empty the input
+        $(".event-input").val("");
+
+        //call the render event function
+        renderEvent();
+
+    });
+
+
+    //if else statement for past, present and future (< = > moment)
+    function timeStyle() {
+        if (
+            $(".saved-events").attr("data-value") <= currentHour) {
+            // change the class to future
+            $(".saved-events").attr("class", "future");
+        }
+        else if (
+            $(".saved-events").attr("data-value") === currentHour) {
+            // change the class to present
+            $(".saved-events").attr("class", "present");
+        }
+
+        else if (
+            $(".saved-events").attr("data-value") <= currentHour) {
+            // change the class to past
+            $(".saved-events").attr("class", "past");
+        }
+
+        console.log("changing the time style");
+    }
+
+    timeStyle();
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,7 @@
+Work Day Scheduler
+
+Shows the current date at the top
+
+Click on an empty block to type an event, then click the "Add Event" button. Your event will be stored in local storage and stored in the event block.
+
+**** This will only work for the 9am event block...still working on the rest.
